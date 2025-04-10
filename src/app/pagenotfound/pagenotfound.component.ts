23108674dc91fa38c7a6e531b02c59461d57c974
d1solis <d1solis@ucsd.edu>
2025-04-10 14:30:51 -0700

    Pagenotfound page complete, minor edits made for aesthetic

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pagenotfound',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './pagenotfound.component.html',
   styleUrl: './pagenotfound.component.css'
 })
 export class PagenotfoundComponent {
+  constructor(private location: Location) {}
 
+     goBack(): void {
+       this.location.back();
+     }
 }
