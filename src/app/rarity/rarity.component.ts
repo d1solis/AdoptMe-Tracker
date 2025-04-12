a92fe15dbd213f8db9a0995e18b5048fd36e43f1
d1solis <d1solis@ucsd.edu>
2025-04-11 23:53:40 -0700

    Rarity page complete, need pages for rarity sections

--- src/app/rarity/rarity.component.ts
+++ src/app/rarity/rarity.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-rarity',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './rarity.component.html',
   styleUrl: './rarity.component.css'
 })
