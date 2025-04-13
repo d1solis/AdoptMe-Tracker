27e8337ec105d5129914ec5eb4fe8c6e8cb45e90
d1solis <d1solis@ucsd.edu>
2025-04-13 15:07:45 -0700

    Search page complete

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
