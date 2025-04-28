1513a767a89ef7c6b2b32431808e5821a57e178a
d1solis <d1solis@ucsd.edu>
2025-04-27 22:40:27 -0700

    completed origin page, minor edits to currency page

--- src/app/origin/origin.component.ts
+++ src/app/origin/origin.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-origin',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './origin.component.html',
   styleUrl: './origin.component.css'
 })
 export class OriginComponent {
+  myOriginType: string = '';
 
+  constructor() { }
+
+  onSubmit() {
+    console.log('Form submitted!');
+    console.log('Selected Origin Type:', this.myOriginType);
+  }
+  
 }
